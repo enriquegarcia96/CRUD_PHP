01bd9a35f53f1caf16d1052bff1b3426f70d4bc9
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-01-23 18:22:03 -0600

    Login, CRUD completo, Variables de sesion

--- modelos/formularios.modelo.php
+++ modelos/formularios.modelo.php
@@ -29,7 +29,88 @@ class ModeloFormularios {
 		$stmt->close();
 		$stmt = null;
 	}	
-	
+
+
+
+
+
+
+	/*=============================================
+		Seleccionar Registros
+	=============================================*/
+
+	static public function mdlSeleccinarRegistros($tabla, $item, $valor){
+		if ($item == null && $valor == null) {
+		$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+
+		$stmt->execute();
+
+		//devuelve todos los registros de la base.
+		return $stmt-> fetchAll(); 
+			
+		}else{
+
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+
+			$stmt->bindParam(":".$item, $valor, PDO::PARAM_STR);
+			$stmt->execute();
+			return $stmt->fetch();
+		}
+		$stmt->close();
+		$stmt = null();
+
+	}
+
+
+
+	/*=============================================
+		Actualizar Registro
+	=============================================*/
+	static public function mdlActualizarRegistro( $tabla, $datos ){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET nombre=:nombre,email=:email,password=:password WHERE id = :id");
+
+		#bindParam() Vincula una variable de PHP a un parametro de sustitucion con nombre o de signo de interogacion correspondiente de la sentencia SQL que fue usada para preparar la sentencia.
+		$stmt->bindParam(":nombre",$datos["nombre"], PDO::PARAM_STR);
+		$stmt->bindParam(":email",$datos["email"], PDO::PARAM_STR);
+		$stmt->bindParam(":password",$datos["password"], PDO::PARAM_STR);
+		$stmt->bindParam(":id",$datos["id"], PDO::PARAM_INT);
+
+
+		if ($stmt->execute()) {
+			return "ok";
+		}else{
+			print_r(Conexion::conectar()->errorInfo());
+		}
+		#para asegurar el sistema
+		$stmt->close();
+		$stmt = null;
+	}	
+
+
+
+
+
+	/*=============================================
+		Eliminar Registro
+	=============================================*/
+	static public function mdlEliminarRegistro( $tabla, $valor ){
+
+		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
+
+		#bindParam() Vincula una variable de PHP a un parametro de sustitucion con nombre o de signo de interogacion correspondiente de la sentencia SQL que fue usada para preparar la sentencia.
+		$stmt->bindParam(":id",$valor, PDO::PARAM_INT);
+
+		if ($stmt->execute()) {
+			return "ok";
+		}else{
+			print_r(Conexion::conectar()->errorInfo());
+		}
+		#para asegurar el sistema
+		$stmt->close();
+		$stmt = null;
+	}	
+
 	
 }
 
--- vistas/paginas/inicio.php
+++ vistas/paginas/inicio.php
@@ -1,26 +1,68 @@
+<?php
+if (!isset($_SESSION["validarIngreso"])) {
+	echo '<script>window.location = "index.php?pagina=ingreso";</script>';
+	return;
+}else{
+	if ($_SESSION["validarIngreso"] != "ok") {
+		echo '<script>window.location = "index.php?pagina=ingreso";</script>';
+		return;
+	}
+}
+	$usuarios = ControladorFormularios::crtSeleccionarRegistros(null, null);
+
+
+	
+
+?>
+
+
 <table class="table table-dark table-striped">
 	<thead>
 		<tr>
-			<th>Firstname</th>
-			<th>Lastname</th>
+			<th>#</th>
+			<th>Nombre</th>
 			<th>Email</th>
+			<th>Acciones</th>
 		</tr>
 	</thead>
 	<tbody>
-		<tr>
-			<td>John</td>
-			<td>Doe</td>
-			<td>john@example.com</td>
+
+		<?php foreach ($usuarios as $key => $value): ?>
+			<tr>
+				<td><?php echo ($key+1); ?></td>
+				<td><?php echo $value["nombre"]; ?></td>
+				<td><?php echo $value["email"]; ?></td>
+				
+			
+			<td>
+				<div class="btn-group">
+
+					<div class="px-1">
+						
+						<a href="index.php?pagina=editar&id=<?php echo $value["id"] ?>" class="btn btn-warning"><i class="fas fa-pen-square"></i></a>
+
+					</div>
+						<form method ="post">
+							<input type="hidden" value="<?php echo $value["id"]; ?>" name="eliminarRegistro">
+
+							<button type="submit" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+
+							<?php 
+								$eliminar = new ControladorFormularios();
+								$eliminar->crtEliminarRegistro();
+
+							 ?>
+
+
+						</form>
+				</div>
+
+
+			</td>
 		</tr>
-		<tr>
-			<td>Mary</td>
-			<td>Moe</td>
-			<td>mary@example.com</td>
-		</tr>
-		<tr>
-			<td>July</td>
-			<td>Dooley</td>
-			<td>july@example.com</td>
-		</tr>
+		<?php endforeach ?>
+
+
+		
 	</tbody>
 </table>
